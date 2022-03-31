4e2db297a46c723b204b31a13f6c131b1a5f63fc
not pug!! <67571892+d101ak11@users.noreply.github.com>
2022-04-01 01:30:58 +0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// slide
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName("img");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(cityName).style.display = "block";  
+}
+function tru(){
+  result = document.getElementById('number'); 
+  var qty = result.value; if( !isNaN(qty)) 
+  result.value++;
+  return false;
+}
